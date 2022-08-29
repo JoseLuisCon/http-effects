e76cb2bb44484bc15df2a68643cb5badd4b606fb
JoseLuisCon <condepa_72@icloud.com>
2022-08-29 17:29:23 +0200

    actulizado componentes lista

--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styles: [
-  ]
+  styleUrls: ['./lista.component.css']
 })
-export class ListaComponent implements OnInit {
+export class ListaComponent implements OnInit, AfterViewInit {
 
   listaUsuarios: Usuario[]=[];
+
+  @ViewChild('grid') refGrid!: HTMLDivElement;
   
-  constructor(private usuariosService: UsuariosService) { }
+  constructor(private usuariosService: UsuariosService) { 
+
+  }
+
+
+  ngAfterViewInit(): void {
+    
+    
+    
+  
+  }
 
   ngOnInit(): void {
     this.usuariosService.getUsers().subscribe ( (lista) =>
       this.listaUsuarios = lista
       )
+
+      
+      
   }
 
 }
